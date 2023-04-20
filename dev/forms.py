abd07528adf074d84cc00612a8725bc8dee50857
Jake Connolly <jake.connolly89@gmail.com>
2023-04-20 12:56:30 -0400

    added form fields for device, account, and login

--- dev/forms.py
+++ dev/forms.py
@@ -2,5 +2,15 @@ from django import forms
 
 class CreateDevice(forms.Form):
     device_name = forms.CharField(max_length=15,label="Name")
-    gps_latitude = forms.DecimalField()
-    gps_longtitude = forms.DecimalField()
+    gps_latitude = forms.DecimalField(decimal_places=2)
+    gps_longtitude = forms.DecimalField(decimal_places=2)
+
+class Login(forms.Form):
+    username = forms.CharField(max_length=15,label="User")
+    password = forms.CharField(max_length=15,label="Pass")
+
+class Register_Account(forms.Form):
+    username = forms.CharField(max_length=15,label="User")
+    password = forms.CharField(max_length=15,label="Pass")
+    confirm_pass = forms.CharField(max_length=15,label="Confirm Pass")
+    email = forms.CharField(max_length=25,label="Email")
